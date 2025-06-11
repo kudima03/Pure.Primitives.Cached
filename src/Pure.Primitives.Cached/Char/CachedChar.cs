f2f4b4a848bbc3130e01527995e0a45592dc601e
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 13:00:34 +0300

    Implemented CachedChar.

---
+++ src/Pure.Primitives.Cached/Char/CachedChar.cs
@@ -0,0 +1,27 @@
+﻿using Pure.Primitives.Abstractions.Char;
+
+namespace Pure.Primitives.Cached.Char;
+
+public sealed record CachedChar : IChar
+{
+    private readonly Lazy<char> _lazyValue;
+
+    public CachedChar(IChar value) : this(new Lazy<char>(() => value.CharValue, LazyThreadSafetyMode.ExecutionAndPublication)) { }
+
+    private CachedChar(Lazy<char> lazyValue)
+    {
+        _lazyValue = lazyValue;
+    }
+
+    char IChar.CharValue => _lazyValue.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
