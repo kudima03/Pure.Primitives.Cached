ec9e5449369743f83659fd6dce0e2e1041f773f4
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 14:25:32 +0300

    Implemented tests for CachedGuid.

---
+++ src/Tests/Pure.Primitives.Cached.Tests/Fakes/GuidWithEvaluationCounter.cs
@@ -0,0 +1,24 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+
+namespace Pure.Primitives.Cached.Tests.Fakes;
+
+public sealed record GuidWithEvaluationCounter : IGuid
+{
+    private readonly System.Guid _value;
+
+    public GuidWithEvaluationCounter(System.Guid value)
+    {
+        _value = value;
+    }
+
+    public int AccessCounter { get; private set; }
+
+    System.Guid IGuid.GuidValue
+    {
+        get
+        {
+            AccessCounter++;
+            return _value;
+        }
+    }
+}
