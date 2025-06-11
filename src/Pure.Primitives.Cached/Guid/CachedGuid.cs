de9d3356de9f995e6fd652105d68bb425731f5f0
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 14:19:22 +0300

    Implemented CachedGuid.

---
+++ src/Pure.Primitives.Cached/Guid/CachedGuid.cs
@@ -0,0 +1,29 @@
+﻿using Pure.Primitives.Abstractions.Guid;
+
+namespace Pure.Primitives.Cached.Guid;
+
+public sealed record CachedGuid : IGuid
+{
+    private readonly Lazy<System.Guid> _lazyValue;
+
+    public CachedGuid(IGuid value) :
+        this(new Lazy<System.Guid>(() => value.GuidValue, LazyThreadSafetyMode.ExecutionAndPublication))
+    { }
+
+    private CachedGuid(Lazy<System.Guid> lazyValue)
+    {
+        _lazyValue = lazyValue;
+    }
+
+    System.Guid IGuid.GuidValue => _lazyValue.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
