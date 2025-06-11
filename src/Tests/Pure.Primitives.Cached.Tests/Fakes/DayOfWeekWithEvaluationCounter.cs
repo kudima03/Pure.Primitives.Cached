0c6f84368879f741700f3b10907667bba372e942
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 14:36:36 +0300

    Implemented tests for CachedDayOfWeek.

---
+++ src/Tests/Pure.Primitives.Cached.Tests/Fakes/DayOfWeekWithEvaluationCounter.cs
@@ -0,0 +1,25 @@
+﻿using Pure.Primitives.Abstractions.DayOfWeek;
+using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Cached.Tests.Fakes;
+
+public sealed record DayOfWeekWithEvaluationCounter : IDayOfWeek
+{
+    private readonly IDayOfWeek _value;
+
+    public DayOfWeekWithEvaluationCounter(IDayOfWeek value)
+    {
+        _value = value;
+    }
+
+    public int AccessCounter { get; private set; }
+
+    INumber<ushort> IDayOfWeek.DayNumberValue
+    {
+        get
+        {
+            AccessCounter++;
+            return _value.DayNumberValue;
+        }
+    }
+}
