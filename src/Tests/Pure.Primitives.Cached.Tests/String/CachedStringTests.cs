b0f417275e8930a31f23856de2dd57e863640182
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 12:39:17 +0300

    Improved tests for CachedString.

--- src/Tests/Pure.Primitives.Cached.Tests/String/CachedStringTests.cs
+++ src/Tests/Pure.Primitives.Cached.Tests/String/CachedStringTests.cs
@@ -1,12 +1,26 @@
-﻿using Pure.Primitives.Abstractions.String;
+﻿using Pure.Primitives.Abstractions.Char;
+using Pure.Primitives.Abstractions.String;
 using Pure.Primitives.Cached.String;
 using Pure.Primitives.Cached.Tests.Fakes;
 using Pure.Primitives.String;
+using System.Collections;
 
 namespace Pure.Primitives.Cached.Tests.String;
 
 public sealed record CachedStringTests
 {
+    [Fact]
+    public void EnumeratesAsUntyped()
+    {
+        const string expected = "expected value";
+        StringWithEvaluationCounter underlyingValue = new StringWithEvaluationCounter(expected);
+        IEnumerable cached = new CachedString(underlyingValue);
+
+        char[] chars = (from IChar i in cached select i.CharValue).ToArray();
+
+        Assert.Equal(expected, new string(chars));
+    }
+
     [Fact]
     public void UnderlyingValueEvaluatesOnce()
     {
