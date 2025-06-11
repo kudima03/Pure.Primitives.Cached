15df0159b01f4502f8ab89d18ac0700f6e79188b
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 13:24:46 +0300

    Implemented tests for CachedNumber<T>.

---
+++ src/Tests/Pure.Primitives.Cached.Tests/Fakes/NumberWithEvaluationCounter.cs
@@ -0,0 +1,24 @@
+﻿using Pure.Primitives.Abstractions.Number;
+
+namespace Pure.Primitives.Cached.Tests.Fakes;
+
+public sealed record NumberWithEvaluationCounter<T> : INumber<T> where T : System.Numerics.INumber<T>
+{
+    private readonly T _value;
+
+    public NumberWithEvaluationCounter(T value)
+    {
+        _value = value;
+    }
+
+    public int AccessCounter { get; private set; }
+
+    T INumber<T>.NumberValue
+    {
+        get
+        {
+            AccessCounter++;
+            return _value;
+        }
+    }
+}
