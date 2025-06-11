36c54499a46adfa6be527dd63684713afc44538e
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 12:31:48 +0300

    Implemented tests for CachedString.

---
+++ src/Tests/Pure.Primitives.Cached.Tests/String/CachedStringTests.cs
@@ -0,0 +1,33 @@
+﻿using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.Cached.String;
+using Pure.Primitives.Cached.Tests.Fakes;
+using Pure.Primitives.String;
+
+namespace Pure.Primitives.Cached.Tests.String;
+
+public sealed record CachedStringTests
+{
+    [Fact]
+    public void UnderlyingValueEvaluatesOnce()
+    {
+        StringWithEvaluationCounter underlyingValue = new StringWithEvaluationCounter(string.Empty);
+        IString cached = new CachedString(underlyingValue);
+        foreach (int i in Enumerable.Range(0, 100))
+        {
+            _ = cached.TextValue;
+        }
+
+        Assert.Equal(1, underlyingValue.AccessCounter);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new CachedString(new EmptyString()).GetHashCode());
+    }
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new CachedString(new EmptyString()).ToString());
+    }
+}
