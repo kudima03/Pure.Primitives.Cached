72b42e876a08398c0ad25d181bd73746eb5999bb
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-11 14:30:07 +0300

    Implemented CachedDayOfWeek.

---
+++ src/Pure.Primitives.Cached/DayOfWeek/CachedDayOfWeek.cs
@@ -0,0 +1,31 @@
+﻿using Pure.Primitives.Abstractions.DayOfWeek;
+using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Cached.Number;
+
+namespace Pure.Primitives.Cached.DayOfWeek;
+
+public sealed record CachedDayOfWeek : IDayOfWeek
+{
+    private readonly Lazy<INumber<ushort>> _lazyValue;
+
+    public CachedDayOfWeek(IDayOfWeek value) : this(new Lazy<INumber<ushort>>(
+        () => new CachedNumber<ushort>(value.DayNumberValue), LazyThreadSafetyMode.ExecutionAndPublication))
+    { }
+
+    private CachedDayOfWeek(Lazy<INumber<ushort>> lazyValue)
+    {
+        _lazyValue = lazyValue;
+    }
+
+    INumber<ushort> IDayOfWeek.DayNumberValue => _lazyValue.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
